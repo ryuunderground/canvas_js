e654c7a29f3b83c49d0faea9e8a64edd210aa40b
ryuunderground <jaeking00001@gmail.com>
2025-10-20 11:53:48 +0900

    house

--- app.js
+++ app.js
@@ -4,10 +4,21 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-// fillRect은 .fill();의 영향을 안 받음음
-ctx.moveTo(65,100);
-ctx.lineTo(132, 165);
-ctx.lineTo(12, 265);
-ctx.lineTo(45, 115);
-ctx.lineTo(65,100);
-ctx.stroke();
+// 벽
+// fill: 다 채움
+// stroke: 테두리만
+ctx.fillRect(250, 300, 25, 200);
+ctx.fillRect(550, 300, 25, 200);
+
+// 문
+// 선 굵기 변경 먼저
+ctx.lineWidth = 3;
+ctx.strokeRect(400, 420, 30, 75);
+
+// 지붕
+ctx.fillRect(250, 300, 300, 20)
+ctx.moveTo(200, 320);
+ctx.lineTo(412.5, 195);
+ctx.lineTo(625, 320);
+ctx.lineTo(200, 320);
+ctx.fill();
