b6c1e1670b9f06ad86e38f65339e43804c0f9c7b
ryuunderground <jaeking00001@gmail.com>
2025-10-20 11:23:46 +0900

    first rect

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-alert("hello");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 800;
+
+ctx.fillRect(100, 50, 150, 270);
